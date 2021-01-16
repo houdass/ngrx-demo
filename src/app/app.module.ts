7616a4a01b6088b6920d0c49d248753b17608025
HOUDASS Youness <youness.houdass@sicpa.com>
2021-01-16 09:50:55 +0100

    5. DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoInfoComponent } from './todo/todo-info/todo-info.component';
 import { todoReducer } from './todo/todo.reducers';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoInfoComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, StoreModule.forRoot({ todo: todoReducer })],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ todo: todoReducer }),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 10 }) : [],
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
